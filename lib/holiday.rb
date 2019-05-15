4e2b8ae61116d79505da6b41ef41e41cfc0846c8
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-15 00:07:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, holiday|
     bbq_holidays = holiday.map do |array|
-      incldue? "BBQ"
+      include? "BBQ"
     end
   end
 end
