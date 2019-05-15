adafa9e04582a399a77929317b439e814e2513d9
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-15 00:08:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,8 +79,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season, holiday|
-    bbq_holidays = holiday.map do |array|
-      include? "BBQ"
+    bbq_holidays = holiday.include?("BBQ") do |array|
     end
   end
 end
