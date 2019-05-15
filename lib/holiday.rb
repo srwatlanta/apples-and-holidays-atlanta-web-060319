b7ce2ca88114922a3b29a7e5776de2bf019dc2c9
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-15 00:35:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -5,12 +5,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash.each do |season, holiday|
-    if season == :winter
-      holiday.each do |array|
-        array << supply
-      end
-    end
+  holiday_hash[:winter].each do |k, v|
+    v << supply
   end
 end
 
